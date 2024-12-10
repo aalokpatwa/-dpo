Fixed logprobs calc

GPT2/dpo.py
@@ -1,29 +1,58 @@
 import torch.nn.functional as F
+import torch
 
-BETA = 0.3
+BETA = 0.1
 
-def logprobs(logits, targets, mask):
+def logprobs(logits, labels, mask):
     # Logits will be (B * T * V)
     # Targets will be (B * T)
-    targets = targets[:, 1:]
+    labels = labels[:, 1:].clone()
+
+    # Truncate logits to match the labels num_tokens
     logits = logits[:, :-1, :]
-    selection_mask = mask[:, 1:].clone()
-    
-    logprobs = F.log_softmax(logits, dim=-1).transpose(1, 2)
-        
-    logprobs = F.cross_entropy(logprobs, targets, reduction='none')
-    
-    logprobs = logprobs * selection_mask
-    
-    logprobs = logprobs.sum(dim=-1)
-        
-    return logprobs
+
+    log_probs = F.log_softmax(logits, dim=-1)
+
+    # Gather the log probabilities for the actual labels
+    selected_log_probs = torch.gather(
+        input=log_probs,
+        dim=-1,
+        index=labels.unsqueeze(-1)
+    ).squeeze(-1)
+
+    if mask is not None:
+        mask = mask[:, 1:].clone()
+
+        # Apply the mask to filter out padding tokens
+        selected_log_probs = selected_log_probs * mask
+
+        # Calculate the average log probability excluding padding tokens
+        # This averages over the tokens, so the shape is (batch_size, num_tokens)
+        avg_log_prob = selected_log_probs.sum(-1) / mask.sum(-1)
+
+        return avg_log_prob
+
+    else:
+        return selected_log_probs.mean(-1)
     
 
 def dpo_loss(model_chosen_logits, model_rejected_logits, reference_chosen_logits, reference_rejected_logits):
-    chosen_rewards = BETA * (model_chosen_logits - reference_chosen_logits)
-    rejected_rewards = BETA * (model_rejected_logits - reference_rejected_logits)
+    model_logratios = model_chosen_logits - model_rejected_logits
+    reference_logratios = reference_chosen_logits - reference_rejected_logits
+    logits = model_logratios - reference_logratios
+
+    # DPO (Eq. 7 of https://arxiv.org/pdf/2305.18290.pdf)
+    losses = -F.logsigmoid(BETA * logits)
+
+    # Optional values to track progress during training
+    chosen_rewards = (model_chosen_logits - reference_chosen_logits).detach()
+    rejected_rewards = (model_rejected_logits - reference_rejected_logits).detach()
+
+    # .mean() to average over the samples in the batch
+    return losses.mean(), chosen_rewards.mean(), rejected_rewards.mean()
+    # chosen_rewards = BETA * (model_chosen_logits - reference_chosen_logits)
+    # rejected_rewards = BETA * (model_rejected_logits - reference_rejected_logits)
     
-    loss = -1 * F.logsigmoid((chosen_rewards - rejected_rewards)).mean()
-    return loss, chosen_rewards.mean(), rejected_rewards.mean()
+    # loss = -1 * F.logsigmoid((chosen_rewards - rejected_rewards)).mean()
+    # return loss, chosen_rewards.mean(), rejected_rewards.mean()
     

GPT2/model.py
@@ -303,13 +303,12 @@ class GPT(nn.Module):
         return mfu
 
     @torch.no_grad()
-    def generate(self, idx, max_new_tokens=30, temperature=0.7, top_k=None):
+    def generate(self, idx, max_new_tokens=30, temperature=1.0, top_k=30):
         """
         Take a conditioning sequence of indices idx (LongTensor of shape (b,t)) and complete
         the sequence max_new_tokens times, feeding the predictions back into the model each time.
         Most likely you'll want to make sure to be in model.eval() mode of operation for this.
         """
-        print (max_new_tokens)
         for _ in range(max_new_tokens):
             # if the sequence context is growing too long we must crop it at block_size
             idx_cond = idx if idx.size(1) <= self.config.block_size else idx[:, -self.config.block_size:]

GPT2/train.py
@@ -11,7 +11,7 @@ import numpy as np
 
 BATCH_SIZE = 8
 EPOCHS = 5
-LEARNING_RATE = 1e-4
+LEARNING_RATE = 1e-5
 
 WEIGHTS_FILE = "gpt2-pytorch_model.bin"
 
@@ -63,7 +63,7 @@ def test_samples(prompts, model, enc, device):
         encoded = enc.encode(text)
         context = torch.tensor(encoded, device=device, dtype=torch.long).unsqueeze(0)
         completion = model.generate(context)
-        out = completion[0, len(completion):].tolist()
+        out = completion[0, :].tolist()
         out = enc.decode(out)
         print (f"Prompt: {text}")
         print (f"Completion: {out}")
@@ -79,13 +79,11 @@ def train(train_loader, val_loader, model, reference, optimizer, enc, device):
     scheduler = CosineAnnealingLR(optimizer, T_max = num_batches * EPOCHS, eta_min=1e-7)
     
     for epoch in range(1, EPOCHS + 1):
-        
         model.eval()
         prompts = ["The morning started with a surprise as", "The calm before the storm"]
         test_samples(prompts, model, enc, device)
         
         model.train()
-        
         for i, batch in enumerate(train_loader):
             optimizer.zero_grad()
 
@@ -93,7 +91,8 @@ def train(train_loader, val_loader, model, reference, optimizer, enc, device):
             loss, chosen_reward, rejected_reward = process_batch(batch, model, reference, device)
             
             print (f"Train Loss: {loss.item()}")
-            print (f"R margin: {chosen_reward - rejected_reward}")
+            print (f"Chosen reward: {chosen_reward}")
+            print (f"Rejected reward: {rejected_reward}")
             
             losses.append(loss.item())
             